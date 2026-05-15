dd7a141f2cd7eac34950c1054b61f59cde6dda99
kailane <kailane.lisley0612@gmail.com>
2026-05-15 17:04:20 -0300

    Ajuste finos

--- guia-documentos/App.js
+++ guia-documentos/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ListaScreen from './src/screens/ListaScreen';
+import ListaScreen from './scr/screens/ListaScreen.js';
 
 export default function App() {
   return (
