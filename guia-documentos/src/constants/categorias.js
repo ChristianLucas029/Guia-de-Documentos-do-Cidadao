fac3bc125c6d0d5e45fb93210b79585763c3ec83
kailane <kailane.lisley0612@gmail.com>
2026-05-22 10:42:41 -0300

    Criando categorias

---
+++ guia-documentos/src/constants/categorias.js
@@ -0,0 +1,14 @@
+export const CATEGORIAS = [
+  'Todas',
+  'Identificação',
+  'Habilitação',
+  'Trabalho',
+  'Viagem',
+];
+
+export const CATEGORIA_CONFIG = {
+  Identificação: { icone: 'badge',          cor: '#2196F3' },
+  Habilitação:   { icone: 'directions-car', cor: '#FF9800' },
+  Trabalho:      { icone: 'work',           cor: '#4CAF50' },
+  Viagem:        { icone: 'flight',         cor: '#9C27B0' },
+};
