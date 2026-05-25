438cd24e6d3488e98d3d0c613790186672f1dd58
Gustavo Antônio <148576864+gustavoantonio03@users.noreply.github.com>
2026-05-25 22:23:03 +0000

    correção: nome da função

--- guia-documentos/src/screens/DicasScreen/index.js
+++ guia-documentos/src/screens/DicasScreen/index.js
@@ -9,7 +9,7 @@ export default function DicasScreen() {
   );
 }
 
-const styles = StyleSheet.define({
+const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5F7FA' },
   texto:     { fontSize: 18, fontWeight: 'bold', color: '#1565C0' },
 });
